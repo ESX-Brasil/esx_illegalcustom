c13c25c82c3ffd6cadd328b2ec76878ffdc29336
renildomarcio <krsolucoesweb@gmail.com>
2018-04-11 04:50:37 -0300

    Initial commit

---
+++ locales/br.lua
@@ -0,0 +1,15 @@
+Locales['br'] = {
+
+	['by_default'] = 'Para padrão',
+	['installed'] = 'instalado',
+	['already_own'] = 'você já tem: ~b~',
+	['not_enough_money'] = 'você não tem dinheiro sujo suficiente!',
+	['purchased'] = 'compra feita!',
+	['press_custom'] = 'pressione ~INPUT_PICKUP~ para personalizar o veículo.',
+	['level'] = 'nível ',
+-- Upgrades
+	['upgrades'] = 'upgrades',
+	['armor'] = 'armadura',
+	['turbo'] = 'turbo',
+	['no_turbo'] = 'sem turbo',
+}
